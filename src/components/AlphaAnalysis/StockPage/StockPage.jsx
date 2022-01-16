5b1fa28ba5da262a68e289c355facd1c904647b2
GeneralLobster <llim8888@hotmail.com>
2022-01-15 16:01:29 -0800

    added stock page gifs and updated stock section

--- src/components/AlphaAnalysis/StockPage/StockPage.jsx
+++ src/components/AlphaAnalysis/StockPage/StockPage.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import "./StockPage.scss"
 import Logo from "../../../assets/Alpha.png"
+import Demo from "../../../assets/StockPage.gif"
+import compareDemo from "../../../assets/CompareStockPage.gif"
 
 export default function StockPage() {
     return (
         <div className="StockPage" id="StockPage">
-            <div className="left">
-                <div className="itemContainer">
-                    <img src={Logo} alt="" className='logo' />
+            <div className="wrapper">
+                <div className="left">
+                    <div className="itemContainer">
+                        <img src={Demo} alt="" className='logo' />
+                    </div>
                 </div>
-            </div>
-            <div className="right">
-                <div className="itemContainer">
-                    <img src={Logo} alt="" className='logo' />
-                    <h2>This is description 1</h2>
+                <div className="left">
+                    <div className="itemContainer">
+                        <img src={compareDemo} alt="" className='logo' />
+                    </div>
                 </div>
-                <div className="itemContainer">
-                    <img src={Logo} alt="" className='logo' />
-                    <h2>This is description 2</h2>
-                </div>
-                <div className="itemContainer">
-                    <img src={Logo} alt="" className='logo' />
-                    <h2>This is description 3</h2>
-                </div>
+                <div className="right">
+                    <div className="itemContainer">
+                        <img src={Logo} alt="" className='logo' />
+                        <h2>This is description 1</h2>
+                    </div>
+                    <div className="itemContainer">
+                        <img src={Logo} alt="" className='logo' />
+                        <h2>This is description 2</h2>
+                    </div>
+                    <div className="itemContainer">
+                        <img src={Logo} alt="" className='logo' />
+                        <h2>This is description 3</h2>
+                    </div>
 
+                </div>
             </div>
         </div>
     )
