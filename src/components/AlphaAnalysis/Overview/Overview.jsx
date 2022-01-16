1e7ec5878aae314ab8bd61d1761778f41fff9314
Andre <acortes417@gmail.com>
2022-01-15 19:04:29 -0800

    andres new changes

--- src/components/AlphaAnalysis/Overview/Overview.jsx
+++ src/components/AlphaAnalysis/Overview/Overview.jsx
@@ -8,6 +8,7 @@ export default function Overview() {
                     Built to help Investors
                 </h1>
             </div>
+
             <div className="bottom">
                 {/* <img src="https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q=" /> */}
                 <ul>
