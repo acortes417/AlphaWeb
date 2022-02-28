47b006581348c68fa6bd908f337c39d6230cbb2f
Andre <acortes417@gmail.com>
2022-02-27 19:30:35 -0800

    fully responsive

--- src/components/AlphaAnalysis/Portfolios/Portfolios.jsx
+++ src/components/AlphaAnalysis/Portfolios/Portfolios.jsx
@@ -12,7 +12,7 @@ export default function Portfolio() {
                 <h1>Meet the Developers</h1>
             </div>
             <div className="wrapper">
-                <Link to="/#/portfolio/Andre" className="andre" style={styles.link}>
+                <Link to="/Andre" className="andre" style={styles.link}>
                     <div className="avatar">
                         <div className="top">
                             <img className="avatar-image" alt="" src="https://media-exp1.licdn.com/dms/image/C5603AQHUAj2mtcsRYw/profile-displayphoto-shrink_200_200/0/1615051317515?e=1646870400&v=beta&t=Ac7CRjX70540pfqezqHMHEOdvasDgHXBpGgoo9MDkC8" />
